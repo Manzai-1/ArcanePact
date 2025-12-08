8806e5fc37e364670ec3fdd58ffd051849e6349d
manzai-1 <0xdecentmaz@gmail.com>
2025-12-08 12:33:57 +0100

    Add invitePlayers function and tests suite

--- smart-contracts/contracts/ArcanePact.sol
+++ smart-contracts/contracts/ArcanePact.sol
@@ -14,6 +14,7 @@ contract ArcanePact {
     }
 
     enum PlayerState {
+        NotAdded,
         Applied,
         PendingSignature,
         Signed,
@@ -39,26 +40,62 @@ contract ArcanePact {
     }
     
     mapping(uint256 => Campaign) internal campaigns;
+    mapping(uint256 => mapping(address => Player)) internal campaignPlayers;
 
     event NewCampaignCreated(
-        uint256 indexed id, 
+        uint256 indexed campaignId, 
         address indexed owner, 
         NewCampaignConfig config
     );
+
+    event NewInvitationAdded(
+        uint256 indexed campaignId,
+        address player
+    );
+
+    error NotOwner(address caller);
+    error CampaignClosedForInvites(uint256 campaignId);
+    error CampaignDoesNotExist(uint256 campaignId);
+    error PlayerAlreadyExists(uint256 campaignId, address player);
 
 
     constructor () {
         nextId = 1;
     }
 
-    function newCampaign (NewCampaignConfig calldata config) external {
-        uint256 id = nextId++;
-        Campaign storage campaign = campaigns[id];
+    function newCampaign (NewCampaignConfig calldata config) external{
+        uint256 campaignId = nextId++;
+        Campaign storage campaign = campaigns[campaignId];
         
         campaign.owner = msg.sender;
         campaign.state = CampaignState.Initialized;
         campaign.inviteOnly = config.inviteOnly;
 
-        emit NewCampaignCreated(id, msg.sender, config);
+        emit NewCampaignCreated(campaignId, msg.sender, config);
+    }
+
+    // use if instead of modifier to avoid looking up mapping twice, add to documentation / natspec later
+    function invitePlayers (uint256 campaignId, address[] calldata addresses) external {
+        Campaign storage campaign = campaigns[campaignId];
+
+        if(campaign.owner == address(0)) 
+            revert CampaignDoesNotExist(campaignId);
+
+        if(campaign.owner != msg.sender) 
+            revert NotOwner(msg.sender);
+
+        if(campaign.state != CampaignState.Initialized) 
+            revert CampaignClosedForInvites(campaignId);
+
+        for(uint256 i = 0; i < addresses.length; i++) {
+            address adr = addresses[i];
+            Player storage player = campaignPlayers[campaignId][adr];
+
+            if(player.state != PlayerState.NotAdded) 
+                revert PlayerAlreadyExists(campaignId, adr);
+                
+            player.state = PlayerState.PendingSignature;
+            emit NewInvitationAdded(campaignId, adr);
+        }
     }
 }
--- smart-contracts/test/ArcanePact.ts
+++ smart-contracts/test/ArcanePact.ts
@@ -19,7 +19,7 @@ async function contractFixture() {
     return { arcanePact, account };
 }
 
-describe('New Campaign', () => {
+describe('Create a new Campaign', () => {
     it('should create a new campaign and emit the correct data', async () => {
         const { arcanePact, account } = await contractFixture();
 
@@ -31,7 +31,7 @@ describe('New Campaign', () => {
                     return true; 
                 },
                 (owner: any) => {
-                    expect(owner).to.equal(account[0]);
+                    expect(owner).to.equal(account[0].address);
                     return true;
                 },
                 (emittedConfig: any) => {
@@ -42,5 +42,72 @@ describe('New Campaign', () => {
                 }
             )
     });
+})
 
+describe('Add players to Campaign', () => {
+    it('should take a (campignId, address[]) and emit a invitation to the campaign for each address when one address', async () => {
+        const { arcanePact, account } = await contractFixture();
+        const players = [account[1].address];
+
+        await arcanePact.newCampaign(campaignConfig);
+        await expect(arcanePact.invitePlayers(1, players))
+            .to.emit(arcanePact, "NewInvitationAdded")
+            .withArgs(1, account[1].address);
+    });
+
+    it('should take a (campignId, address[]) and emit a invitation to the campaign for each address when multiple addresses', async () => {
+        const { arcanePact, account } = await contractFixture();
+        const players = [
+            account[1].address, 
+            account[2].address
+        ];
+
+        await arcanePact.newCampaign(campaignConfig);
+        await expect(arcanePact.invitePlayers(1, players))
+            .to.emit(arcanePact, "NewInvitationAdded").withArgs(1, account[1].address)
+            .and.to.emit(arcanePact, "NewInvitationAdded").withArgs(1, account[1].address);
+    });
+
+    it('should throw a NotOwner error if caller is not owner of the contract', async () => {
+        const { arcanePact, account } = await contractFixture();
+        const players = [account[1].address];
+
+        await arcanePact.newCampaign(campaignConfig);
+        await expect(arcanePact.connect(account[1]).invitePlayers(1, players))
+            .to.be.revertedWithCustomError(arcanePact, "NotOwner")
+            .withArgs(account[1].address);
+    });
+
+    it('should throw a CampaignClosedForInvites error if the campaign is not in state initialized', async () => {
+        const { arcanePact, account } = await contractFixture();
+        const players = [account[1].address];
+
+        await arcanePact.newCampaign(campaignConfig);
+        await expect(arcanePact.invitePlayers(1, players))
+            .to.be.revertedWithCustomError(arcanePact, "CampaignClosedForInvites")
+            .withArgs(1);
+    });
+
+    it('should throw a CampaignDoesNotExist error if the campaign does not exist', async () => {
+        const { arcanePact, account } = await contractFixture();
+        const players = [account[1].address];
+
+        await arcanePact.newCampaign(campaignConfig);
+        await expect(arcanePact.invitePlayers(2, players))
+            .to.be.revertedWithCustomError(arcanePact, "CampaignDoesNotExist")
+            .withArgs(2);
+    });
+
+    it('should throw a PlayerAlreadyExists error if an invited address already exists in relation to the campaign', async () => {
+        const { arcanePact, account } = await contractFixture();
+        const players = [
+            account[1].address,
+            account[1].address
+        ];
+
+        await arcanePact.newCampaign(campaignConfig);
+        await expect(arcanePact.invitePlayers(1, players))
+            .to.be.revertedWithCustomError(arcanePact, "PlayerAlreadyExists")
+            .withArgs(1, account[1].address);
+    });
 })
