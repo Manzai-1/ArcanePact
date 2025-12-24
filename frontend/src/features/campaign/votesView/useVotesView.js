0f908fed43f12aaac639002493aae744a28d2823
manzai-1 <0xdecentmaz@gmail.com>
2025-12-24 03:07:35 +0100

    Fix useVotesView to show correct required votes number

--- frontend/src/features/campaign/votesView/useVotesView.js
+++ frontend/src/features/campaign/votesView/useVotesView.js
@@ -13,9 +13,11 @@ export const useVotesView = (campaign) => {
     if(!sendTx) return null;
 
     const rows = campaign.votes.map((vote,i) => {
+        const count = +campaign.participantCount;
+        const required = Math.floor(count / 2) +1;
+
         const voteName = vote.voteName;
         const voteCount = vote.voteCount;
-        const required = Math.ceil(+campaign.participantCount / 2);
 
         return { voteName, voteCount, required, id: i };
     })
